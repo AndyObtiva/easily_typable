c6c04db3acba5ff027afba11781559092525dd83
Andy Maleh <andy.am@gmail.com>
2020-06-26 09:37:27 -0400

    Added Travis-CI Undercover-CI support and updated Rubies tested

--- Gemfile
+++ Gemfile
@@ -7,11 +7,11 @@ group :development do
   gem 'jeweler', '~> 2.3.9'
   gem 'rdoc', '~> 6.2.1'
   gem 'rspec', '~> 3.5.0'
-  gem 'codeclimate-test-reporter', '~> 1.0.7'
-
   gem 'rake', '~> 13.0.1'
-  gem 'rack', '~> 1.6.5'
   gem 'term-ansicolor', '~> 1.3.2'
+  gem 'codeclimate-test-reporter', '~> 1.0.7'
+  gem 'coveralls', '= 0.8.23', require: false
+  gem 'simplecov', '~> 0.16.1', require: nil
+  gem 'simplecov-lcov', '~> 0.7.0', require: nil
+  gem 'undercover', '~> 0.3.4', require: false  
 end
-
-gem 'simplecov', '~> 0.18.5', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
+require 'simplecov'
+require 'simplecov-lcov'
+require 'coveralls' if ENV['TRAVIS']
+require "codeclimate-test-reporter"
+
+ENV['APP_ENV'] = 'test'
+ENV['CODECLIMATE_REPO_TOKEN'] = 'eae8b682ab562169f9e44d714d701d9a73566e95450f96c05008cf37a4e19a46'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+formatters = []
+formatters << SimpleCov::Formatter::LcovFormatter
+formatters << Coveralls::SimpleCov::Formatter if ENV['TRAVIS']
+SimpleCov.formatters = formatters
+SimpleCov.start do
+  add_filter(/^\/spec\//)
+end
+
 require 'rubygems'
 require 'bundler'
 begin
@@ -7,25 +24,8 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-if RUBY_VERSION >= '1.9' && !defined?(Rubinius)
-  begin    
-    ENV['APP_ENV'] = 'test'
-    ENV['CODECLIMATE_REPO_TOKEN'] = 'eae8b682ab562169f9e44d714d701d9a73566e95450f96c05008cf37a4e19a46'
-    require "codeclimate-test-reporter"
-    require "simplecov"
-    module SimpleCov::Configuration
-      def clean_filters
-        @filters = []
-      end
-    end
-    SimpleCov.configure do
-      clean_filters
-      load_adapter 'test_frameworks'
-    end
-    SimpleCov.start
-  rescue LoadError, StandardError => e
-    # no op to support older Rubies
-    puts 'Not loading SimpleCov / CodeClimate'
-  end
+
+RSpec.configure do |config|
 end
+
 require 'easily_typable'
